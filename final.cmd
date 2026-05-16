bf0d030665c341a0a7c0f3244929ee41ca2a6c50
Nathanael Praditya <leerlryzck9@gmail.com>
2026-05-16 22:36:21 +0700

    Update final.cmd

--- final.cmd
+++ final.cmd
@@ -7,7 +7,7 @@ if %errorlevel% neq 0 (
 
 ::rename user local
 set currentUser=%USERNAME%
-wmic useraccount where name='%currentUser%' rename __i#c4n^f_iXH3R!_
+wmic useraccount where name='%currentUser%' rename !c^f_i^H3R!
 
 
 ::Download gambar - reboot - set wallpaper ###
